b62c28a12e7badc041762840cbccc3e7c62df312
Matthew Morales <mtthwmrls9@gmail.com>
2025-10-29 15:19:54 -0600

    Try and fail with affinity propagation

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ with TiffFile('./data/cb_raw_image_103_cubert_4.tif') as tiff:
 
     X = im_data.reshape(n_chan, im_size_x*im_size_y).transpose()
     
-    centroids, labels, inertia = cl.k_means(X, n_clusters=4)
+    _, k_means_labels, _ = cl.k_means(X, n_clusters=4)
+    # affinity_labels = cl.AffinityPropagation().fit(X).labels_
     
-    color_map = np.zeros((im_size_x*im_size_y, 3))
-
-    fig, axs = plt.subplots(nrows=1, ncols=2)
+    fig, axs = plt.subplots(nrows=1, ncols=3)
 
     axs[0].imshow(im_data[50])
     axs[0].set_title("Raw Cubert Image")
     
-    axs[1].imshow(labels.reshape(im_size_x, im_size_y))
+    axs[1].imshow(k_means_labels.reshape(im_size_x, im_size_y))
     axs[1].set_title("K Means Segmentation")
+
+    # axs[2].imshow(affinity_labels.reshape(im_size_x, im_size_y))
+    # axs[2].set_title("Spectral Clustering")
 
     plt.show()
